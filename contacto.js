8393e4e391921d16102e650ec544381781ae50be
gonzamoro <35476726+gonzamoro@users.noreply.github.com>
2019-06-17 21:04:33 -0300

    Update contacto.js

--- contacto.js
+++ contacto.js
@@ -1,20 +1,27 @@
 'use strict'
 
-var nodemailer = require('nodemailer'),
-    util = require('util'),
-    querystring = require('querystring'),
-    sesion = require('./sesion.json'),
+const nodemailer = require("nodemailer");
+const sesion = require('./sesion.json');
 
+// async..await is not allowed in global scope, must use a wrapper
+async function main(){
 
-    transporter = nodemailer.createTransport({
-      service: sesion.service,
-      auth: {
-      user: sesion.user,
-      pass: sesion.pass
-      }
-}),
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: sesion.user, // generated ethereal user
+      pass: sesion.pass // generated ethereal password
+    }
+  });
     
-    Contacto = () => {}
+        Contacto = () => {}
 
 Contacto.tata = (req, res, next) => {
     let
@@ -22,32 +29,34 @@ Contacto.tata = (req, res, next) => {
       email= req.body.emaill,
       telefono= req.body.telefonoo,
       mensaje= req.body.mensaje,
-      mailOptions = {
-      from: 'test.md136@gmail.com',
-      to: 'test.md136@gmail.com',
-      subject: 'Sending Email using Node.js',
-      text:`
+      text =`
             nombre: ${nombre}
             e-mail: ${email}
             telefono: ${telefono}
             mensaje: ${mensaje}
-            `
-}
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-    console.log(req.params);
-  }
-  res.redirect('/')
-});
-
-
+            `;
 }
 
-module.exports = Contacto 
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'test.md136@gmail.com', // sender address
+    to: 'test.md136@gmail.com', // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: `
+            nombre: ${nombre}
+            e-mail: ${email}
+            telefono: ${telefono}
+            mensaje: ${mensaje}
+            `, // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
 
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
 
-
+main().catch(console.error);
